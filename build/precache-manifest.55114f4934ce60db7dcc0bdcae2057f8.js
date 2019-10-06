4bb598c45d31ea0b044524a000194aa8900051bb
alexisjcarr <acarr59@gmail.com>
2019-10-06 10:58:41 -0500

    ✅ Adds .travis.yml

---
+++ build/precache-manifest.55114f4934ce60db7dcc0bdcae2057f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4ff44989ef198be6b10702797d10189",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43c8f7083c8f8e408f43",
+    "url": "/static/css/main.e23ac967.chunk.css"
+  },
+  {
+    "revision": "9ff48109c9dfec965ab4",
+    "url": "/static/js/2.0100e745.chunk.js"
+  },
+  {
+    "revision": "43c8f7083c8f8e408f43",
+    "url": "/static/js/main.e0215f22.chunk.js"
+  },
+  {
+    "revision": "d83ff77a7db9d559573b",
+    "url": "/static/js/runtime-main.a7705736.js"
+  }
+]);
