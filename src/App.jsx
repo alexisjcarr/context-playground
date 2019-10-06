11eaac6d5849076d1637084fe7e79fac1370c1b5
alexisjcarr <acarr59@gmail.com>
2019-10-06 00:46:26 -0500

    ♻️ Adds CartContext and refactors app to consume it
     #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Route } from "react-router-dom";
 import data from "./data";
-import { ProductContext } from "./contexts";
+import { ProductContext, CartContext } from "./contexts";
 
 // Components
 import Navigation from "./components/Navigation";
@@ -19,11 +19,11 @@ function App() {
   return (
     <div className="App">
       <ProductContext.Provider value={{ products, addItem }}>
-        <Navigation cart={cart} />
-        {/* Routes */}
-        <Route exact path="/" component={Products} />
-
-        <Route path="/cart" render={() => <ShoppingCart cart={cart} />} />
+        <CartContext.Provider value={{ cart }}>
+          <Navigation />
+          <Route exact path="/" component={Products} />
+          <Route path="/cart" component={ShoppingCart} />
+        </CartContext.Provider>
       </ProductContext.Provider>
     </div>
   );
--- src/contexts/index.js
+++ src/contexts/index.js
@@ -1,3 +1,4 @@
 import { ProductContext } from "./ProductContext";
+import { CartContext } from "./CartContext";
 
-export { ProductContext };
+export { ProductContext, CartContext };
